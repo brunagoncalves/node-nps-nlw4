75692f16b703963b61bfcdcbf74904c9200fae68
Bruna Goncalves <brunagoncalves.sjb@gmail.com>
2021-02-23 11:22:27 -0300

    Initial commit

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
+import 'reflect-metadata';
 import express from 'express';
+
+import './database';
+import router from './router';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({message: 'Olá Mundo!'})
-})
+app.use(express.json());
+app.use(router)
 
 app.listen(3333, () => {
   console.log('Server is running in port 3333')
